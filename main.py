4b425b01de1ee86af19d18f2da9ec7d1e8dfc506
killpeoplewithhammers <123596850+killpeoplewithhammers@users.noreply.github.com>
2023-02-08 15:23:51 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,14 @@
 
 from tkinter import *
 from tkinter import ttk
+import time
+
+clickarray = []
 
 root = Tk()
 frm = ttk.Frame(root, padding=10)
 frm.grid()
-ttk.Label(frm, text=f"Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Click Here", command=root.destroy).grid(column=0, row=2)
+ttk.Label(frm, text=f"you click ").grid(column=0, row=0)
+ttk.Button(frm, text="Click Fast", command=root.destroy, ).grid(column=0, row=1)
 root.mainloop()
+print("time is", time.clock_gettime_ns(time.CLOCK_REALTIME))
