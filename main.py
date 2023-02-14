a24aad6ef6d7d5f6e4971942e8d2bbbfc8800c0b
killpeoplewithhammers <123596850+killpeoplewithhammers@users.noreply.github.com>
2023-02-14 15:21:06 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,23 @@
 
 from tkinter import *
 from tkinter import ttk
+from functools import partial
 import time
 
-clickarray = []
-def click(clickarray):
-    clickarray = clickarray + [time.clock_gettime_ns(time.CLOCK_REALTIME)]
-    print(clickarray)
-
+clickArray = []
+def click(clickArray):
+    currentTime = time.clock_gettime_ns(time.CLOCK_REALTIME)
+    clickArray.append(currentTime)
+    for clickNumber in range(len(clickArray)-1):
+        if clickArray[clickNumber] + 1000000000 < currentTime:
+            clickArray.pop(clickNumber)
+        else:
+            break
+    return clickArray
+    
 root = Tk()
-frm = ttk.Frame(root, padding=10)
+frm = ttk.Frame(root, padding=100)
 frm.grid()
-ttk.Label(frm, text=f"you click ").grid(column=0, row=0)
-ttk.Button(frm, text="Click Fast", command=).grid(column=0, row=1)
+ttk.Label(frm, text=f"you click {len(clickArray)}").grid(column=0, row=0)
+ttk.Button(frm, text="Click Fast", command=partial(click, clickArray)).grid(column=0, row=1)
 root.mainloop()
-print("time is", time.clock_gettime_ns(time.CLOCK_REALTIME)
